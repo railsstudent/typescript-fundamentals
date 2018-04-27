cd286858dc951def9d8409357faeede5e7b76a48
Connie Leung <connie.leung@nixplay.com>
2018-04-27 10:49:27 +0800

    Create shell functions
    Signed-off-by: Connie Leung <connie.leung@nixplay.com>

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,9 @@
 //TODO: Implement hexToRgb
-
+export const hexToRgb = (hex: string) => {
+	return null;
+}
 
 //TODO: Implement rgbToHex
+export const rgbToHex = (r: number, g: number, b: number): string => {
+	return '';
+}
