5d5e846fc929d4e4e228673e49f718d006285c11
Connie Leung <cleung334@gmail.com>
2018-05-03 23:55:22 +0800

    completed solution of exercise 3

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,58 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+type Card = [Suit, CardNumber];
 
+export class Dealer {
+  cards: Card[];
+  constructor() {
+    this.cards = [];
+    for (let i = 0; i < Object.keys(Suit).length / 2; i++) {
+      for (let j = 0; j < Object.keys(CardNumber).length / 2; j++) {
+        this.cards.push([i, j]);
+      }
+    }
+    shuffleArray(this.cards);
+  }
+  getLength(): number {
+    return this.cards.length;
+  }
+
+  readCard(card: Card): string {
+    const [suit, num] = card;
+    return `${CardNumber[num]} of ${Suit[suit]}`;
+  }
+
+  dealHand(num: number): Card[] {
+    if (num < 0 || num > this.getLength()) {
+      throw new Error("Error");
+    }
+
+    const results: Card[] = this.cards.splice(0, num);
+    shuffleArray(this.cards);
+    return results;
+  }
+}
+
+export enum Suit {
+  Clubs = 0,
+  Diamonds = 1,
+  Hearts = 2,
+  Spades = 3
+}
+
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Fifth,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
 }
