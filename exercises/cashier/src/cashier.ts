c4c75eade007246698bcc07332021dfc31b572b1
Connie Leung <cleung334@gmail.com>
2018-05-06 08:26:39 +0800

    Completed solution to exercise 4.

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -7,24 +7,22 @@ interface CartItem {
 interface CartAPI {
   length: number;
   total: number;
-  addItem(item: CartItem): CartAPI;
-  add(name: string, price: number, qty?: number): CartAPI;
+  addItem: (item: CartItem) => CartAPI;
+  add: (name: string, price: number, qty?: number) => CartAPI;
 }
 
 export function cashier(): CartAPI {
-  var items: CartItem[] = [];
+  let items: CartItem[] = [];
 
   return {
     get length() {
       return items.reduce((acc, item: CartItem) => {
-        const { qty = 0 } = item || {};
-        return acc + qty;
+        return acc + item.qty;
       }, 0);
     },
     get total() {
       return items.reduce((acc, item: CartItem) => {
-        const { price = 0, qty = 0 } = item || {};
-        return acc + price * qty;
+        return acc + item.price * item.qty;
       }, 0);
     },
     add(name: string, price: number, qty: number = 1): CartAPI {
@@ -36,28 +34,4 @@ export function cashier(): CartAPI {
       return this;
     }
   };
-
-  // function add(name: string, price: number, qty: number = 1): CartAPI {
-  //   items.push({ name, price, qty });
-  //   return this;
-  // }
-
-  // function addItem(item: CartItem): CartAPI {
-  //   items.push(item);
-  //   return this;
-  // }
-
-  function countQuantity(): number {
-    return items.reduce((acc, item: CartItem) => {
-      const { qty = 0 } = item || {};
-      return acc + qty;
-    }, 0);
-  }
-
-  function calculate(): number {
-    return items.reduce((acc, item: CartItem) => {
-      const { price = 0, qty = 0 } = item || {};
-      return acc + price * qty;
-    }, 0);
-  }
 }
