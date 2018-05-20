b964e0f506225915a2d4d3fc01f961c1bff16d0d
Connie Leung <cleung334@gmail.com>
2018-05-20 11:17:24 +0800

    Completed implementation of typed stack.

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,30 @@ interface IStack<T> {
   print(): void;
 }
 
-export class Stack<T> {
+export class Stack<T> implements IStack<T> {
+  private stack: T[] = [];
 
+  public push(items: T | T[]): IStack<T> {
+    if (Array.isArray(items)) {
+      items.forEach(item => this.stack.push(item));
+    } else {
+      this.stack.push(items);
+    }
+    return this;
+  }
+
+  public pop(): T | undefined {
+    if (this.length() > 0) {
+      return this.stack.pop();
+    }
+    return undefined;
+  }
+
+  public length(): number {
+    return this.stack.length;
+  }
+
+  public print(): void {
+    console.log(JSON.stringify(this.stack));
+  }
 }
